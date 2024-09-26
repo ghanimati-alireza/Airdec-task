71e70a3a2a49a0a71cdee65315499365c6d127ed
alireza ghanimati <alireza.ghanimati78@gmail.com>
2024-09-27 03:00:28 +0330

    Remove 'estimate/' prefix from URLs to reduce redundancy

--- estimate/urls.py
+++ estimate/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import EstimateCreateView, EstimateDetailView
 
 urlpatterns = [
-    path('estimate/', EstimateCreateView.as_view(), name='create-estimate'),
-    path('estimate/<int:pk>/', EstimateDetailView.as_view(), name='estimate-detail'),
+    path('', EstimateCreateView.as_view(), name='create-estimate'),
+    path('<int:pk>/', EstimateDetailView.as_view(), name='estimate-detail'),
 ]
