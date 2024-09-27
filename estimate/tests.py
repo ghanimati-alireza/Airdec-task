2b1fcb8d099f8bf5a24150830a8cf8d4e8c49399
alireza ghanimati <alireza.ghanimati78@gmail.com>
2024-09-27 22:23:04 +0330

    add integration tests

--- estimate/tests.py
+++ estimate/tests.py
@@ -1,4 +1,3 @@
-from django.test import TestCase
 from estimate.models import Estimate
 from rest_framework import status
 from rest_framework.test import APITestCase
@@ -89,4 +88,49 @@ class EstimateDetailViewTestCase(APITestCase):
         response = self.client.delete('/estimate/999/')  # Assuming 999 does not exist
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
-# Create your tests here.
+
+# ==========================
+#     INTEGRATION TESTS
+# ==========================
+class EstimateCreationTestCase(APITestCase):
+    def setUp(self):
+        self.user: User = User.objects.create(email='alireza.ghnaimati78@gmail.com')
+        self.url = '/estimate/'
+        self.client.force_authenticate(user=self.user)
+
+    def test_estimate_creation(self):
+        data = {
+            'note': 'this is test estimate',
+            'created_by': self.user.id,
+        }
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Estimate.objects.count(), 1)
+        self.assertEqual(response.data['note'], data['note'])
+        self.assertEqual(response.data['created_by'], data['created_by'])
+
+
+class EstimateDetailTestCase(APITestCase):
+    def setUp(self):
+        self.user: User = User.objects.create(email='alireza.ghnaimati78@gmail.com')
+        self.estimate: Estimate = Estimate.objects.create(note='Initial note', created_by=self.user)
+        self.url = f'/estimate/{self.estimate.id}/'
+        self.client.force_authenticate(user=self.user)
+
+    def test_retrieve_estimate(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['note'], self.estimate.note)
+        self.assertEqual(response.data['created_by'], self.estimate.created_by)
+
+    def test_update_estimate(self):
+        data = {'note': 'Updated note'}
+        response = self.client.patch(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.estimate.refresh_from_db()
+        self.assertEqual(self.estimate.note, 'Updated note')
+
+    def test_delete_estimate(self):
+        response = self.client.delete(self.url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Estimate.objects.count(), 0)
