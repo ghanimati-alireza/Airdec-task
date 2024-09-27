2fdb95580e4ad8a1aef855a69d0496d951fc38be
alireza ghanimati <alireza.ghanimati78@gmail.com>
2024-09-27 23:58:44 +0330

    Implement soft deletion in BaseModel
    This update modifies the delete functionality for all models by introducing a `deleted_by` field. Instead of permanently deleting records from the database, the soft deletion approach marks them as deleted, preserving data integrity and allowing for potential recovery.

--- utils/models.py
+++ utils/models.py
@@ -1,5 +1,20 @@
 from django.db import models
+from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
+
+
+class SoftDeleteManager(models.Manager):
+    """Manager that retrieves only non-deleted objects."""
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=True)
+
+
+class DeletedManager(models.Manager):
+    """Manager that retrieves only soft-deleted objects."""
+
+    def get_queryset(self):
+        return super().get_queryset().filter(deleted_at__isnull=False)
 
 
 class BaseModel(models.Model):
@@ -9,3 +24,16 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+    objects = SoftDeleteManager()  # Manager for non-deleted objects
+    all_objects = models.Manager()  # Manager for all objects (deleted and non-deleted)
+    deleted_objects = DeletedManager()  # Manager for only soft-deleted objects
+
+    def delete(self, using=None, keep_parents=False):
+        """Soft delete the object by setting the deleted_at field."""
+        self.deleted_at = timezone.now()
+        self.save(update_fields=['deleted_at'])
+
+    def hard_delete(self, using=None, keep_parents=False):
+        """Permanently delete the object."""
+        super().delete(using=using, keep_parents=keep_parents)
