0af27f2f3996d4c4d00eef8fff5c3f9bfddade70
alireza ghanimati <alireza.ghanimati78@gmail.com>
2024-09-28 02:18:44 +0330

    Add authentication and throttling to EstimateCreateView
    - Applied IsAuthenticated permission to restrict access to authenticated users
    - Implemented UserRateThrottle for default throttling behavior
    - Set up automatic assignment of the 'created_by' field to the current user

--- estimate/views.py
+++ estimate/views.py
@@ -1,10 +1,21 @@
-from rest_framework import generics
+from rest_framework import generics, permissions, throttling
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.throttling import ScopedRateThrottle
 from .models import Estimate
 from .serializers import EstimateSerializer
+
 
 class EstimateCreateView(generics.CreateAPIView):
     queryset = Estimate.objects.all()
     serializer_class = EstimateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    throttle_classes = [throttling.UserRateThrottle, ScopedRateThrottle]
+    throttle_scope = 'user_minute'
+
+    def perform_create(self, serializer):
+        # Automatically set the 'created_by' field to the current user
+        serializer.save(created_by=self.request.user)
+
 
 class EstimateDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Estimate.objects.all()
